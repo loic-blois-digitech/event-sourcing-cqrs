308417a4cc7b33c9b7e896fb00c6c58f40230687
Blois <loic.blois@digitech-development.com>
2023-02-09 08:17:12 +0100

    Init app.

---
+++ src/main/java/com/example/eventsourcingcqrs/EventSourcingCqrsApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventsourcingcqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSourcingCqrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventSourcingCqrsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eventsourcingcqrs/EventSourcingCqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventsourcingcqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSourcingCqrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
