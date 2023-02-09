a7e874c4feae98a7cba0662733b7a3c6e5d8ae5d
Blois <loic.blois@digitech-development.com>
2023-02-09 11:07:32 +0100

    Add event handler in principal aggregate.

--- src/main/java/com/example/eventsourcingcqrs/aggregate/BankAccountAggregate.java
+++ src/main/java/com/example/eventsourcingcqrs/aggregate/BankAccountAggregate.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
@@ -34,5 +35,12 @@ public class BankAccountAggregate implements Serializable {
                   command.getOwner()
           )
         );
+    }
+
+    @EventSourcingHandler
+    public void on(AccountCreatedEvent event) {
+        this.id = event.getId();
+        this.owner = event.getOwner();
+        this.balance = event.getInitialBalance();
     }
 }
