11281aeb43813e7f94b085cc2f4a71994d983bfe
Blois <loic.blois@digitech-development.com>
2023-02-09 10:56:30 +0100

    Add principal aggregate BankAccountAggregate.

---
+++ src/main/java/com/example/eventsourcingcqrs/aggregate/BankAccountAggregate.java
@@ -0,0 +1,38 @@
+package com.example.eventsourcingcqrs.aggregate;
+
+import com.example.eventsourcingcqrs.aggregate.event.AccountCreatedEvent;
+import com.example.eventsourcingcqrs.aggregate.target.CreateAccountCommand;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
+import org.axonframework.spring.stereotype.Aggregate;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Aggregate
+public class BankAccountAggregate implements Serializable {
+
+    @AggregateIdentifier
+    private UUID id;
+    private BigDecimal balance;
+    private String owner;
+
+    @CommandHandler
+    public BankAccountAggregate(CreateAccountCommand command) {
+        AggregateLifecycle.apply(
+          new AccountCreatedEvent(
+                  command.getAccountId(),
+                  command.getInitialBalance(),
+                  command.getOwner()
+          )
+        );
+    }
+}
